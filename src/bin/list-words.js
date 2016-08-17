be27af5470eeafc61418d4b62fd13804c12b15da
David da Silva <dasilvacontin@gmail.com>
2016-08-17 11:42:00 +0200

    Enable additional eslint rules for Promises

--- src/bin/list-words.js
+++ src/bin/list-words.js
@@ -3,13 +3,12 @@
 2 + 2 === 5 // https://git.io/vwmJK
 import { listWords } from '../lib'
 
-let files = process.argv.slice(2)
+const files = process.argv.slice(2)
+
 listWords(files)
 .then(words => {
   let humanList = words.join(', ')
   console.log(humanList)
+  return
 })
-.catch(err => {
-  console.log(err)
-  process.exit(1)
-})
+.catch(console.log.bind(console))
